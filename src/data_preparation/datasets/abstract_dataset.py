5ce67b001bcd11603a0b0d8c0d031bb96a0d4ca0
Shakleen Ishfar <sishfar@ur.rochester.edu>
2024-11-07 15:10:08 -0500

    v0.0.7: Abstract dataset class
    All future datasets should inherit from the abstract dataset class.
    It encapsulates the common functionality of a dataset.

---
+++ src/data_preparation/datasets/abstract_dataset.py
@@ -0,0 +1,76 @@
+from abc import ABC, abstractmethod
+from typing import Optional
+
+from torch.utils.data import Dataset
+from transformers import AutoTokenizer
+import pandas as pd
+
+
+class AbstractDataset(Dataset, ABC):
+    def __init__(
+        self,
+        dataframe: pd.DataFrame,
+        tokenizer: AutoTokenizer,
+        question_max_length: Optional[int] = 512,
+        misconception_max_length: Optional[int] = 64,
+        include_meta_data: Optional[bool] = False,
+    ):
+        """Initialize the dataset.
+
+        Args:
+            dataframe (pd.DataFrame): Dataframe containing the dataset.
+            tokenizer (AutoTokenizer): Tokenizer to use for the dataset.
+            question_max_length (Optional[int], optional): Maximum length of the question. Defaults to 512.
+            misconception_max_length (Optional[int], optional): Maximum length of the misconception. Defaults to 64.
+            include_meta_data (Optional[bool], optional): Whether to include meta data. Defaults to False.
+        """
+        self.df = dataframe
+        self.tokenizer = tokenizer
+        self.question_max_length = question_max_length
+        self.misconception_max_length = misconception_max_length
+        self.include_meta_data = include_meta_data
+
+    def __len__(self):
+        """Get the length of the dataset.
+
+        Returns:
+            int: Length of the dataset.
+        """
+        return len(self.df)
+
+    def split_misconception_list(
+        self, misconception_text: str, delimiter: str = "###"
+    ) -> list[str]:
+        """Split a misconception text into a list of misconception texts.
+
+        For example, if the misconception text is "a###b###c", the output will be ["a", "b", "c"].
+
+        Args:
+            misconception_text (str): Text to split.
+
+        Returns:
+            list[str]: List of misconception texts.
+        """
+        misconception_list = misconception_text.split(delimiter)
+        misconception_list = [
+            misconception.strip() for misconception in misconception_list
+        ]
+        return misconception_list
+
+    def parse_list_from_string(self, string: str) -> list[int]:
+        """Parse a list from a string.
+
+        Input: "[1, 2, 3]"
+        Output: [1, 2, 3]
+
+        Args:
+            string (str): String to parse.
+
+        Returns:
+            list[int]: List of integers.
+        """
+        return [int(m) for m in string.replace("[", "").replace("]", "").split(",")]
+
+    @abstractmethod
+    def __getitem__(self, idx):
+        raise NotImplementedError("Subclasses must implement this method")
---
+++ tests/data_preparation/datasets/test_abstract_dataset.py
@@ -0,0 +1,47 @@
+import pytest
+import pandas as pd
+from src.data_preparation.datasets.abstract_dataset import AbstractDataset
+
+
+class Dataset(AbstractDataset):
+    def __getitem__(self, idx):
+        return self.df.iloc[idx]
+
+
+@pytest.fixture
+def dataset():
+    df = pd.DataFrame({"a": [1, 2, 3]})
+    return Dataset(df, None)
+
+
+def test_abstract_dataset():
+    with pytest.raises(TypeError):
+        AbstractDataset()
+
+
+def test_has_abstract_methods():
+    assert AbstractDataset.__abstractmethods__ == {"__getitem__"}
+
+
+def test_len(dataset: Dataset):
+    assert len(dataset) == 3
+
+
+def test_getitem(dataset: Dataset):
+    for i in range(len(dataset)):
+        assert dataset[i]["a"] == i + 1
+
+
+def test_split_misconception_list(dataset: Dataset):
+    delimiter = "###"
+    misconception_text = delimiter.join(["a", "b", "c"])
+    assert dataset.split_misconception_list(misconception_text, delimiter) == [
+        "a",
+        "b",
+        "c",
+    ]
+
+
+def test_parse_list_from_string(dataset: Dataset):
+    string = "[1, 2, 3]"
+    assert dataset.parse_list_from_string(string) == [1, 2, 3]
